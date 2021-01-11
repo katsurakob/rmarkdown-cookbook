39893378fd37fdd985c791ca165066dc0374914e
Katagiri, Satoshi <katagiri.stsh@gmail.com>
2021-01-11 19:32:49 +0900

    environment settings

--- Makefile
+++ Makefile
@@ -1,15 +1,15 @@
 all:
-	rm -f _book/*.html &&\
+	rm -f docs/*.html &&\
 	BOOKDOWN_FULL_PDF=false Rscript --quiet _render.R
 
 pdf:
-	Rscript --quiet _render.R "bookdown::pdf_book" &&\
+	Rscript --quiet _render.R "rmdja::pdf_book_ja" &&\
 	rm -f css/box.css latex/blackbox.tex latex/infobox.tex &&\
-	mv _book/rmarkdown-cookbook.pdf _book/rmarkdown-cookbook-full.pdf &&\
-	open _book/rmarkdown-cookbook-full.pdf
+	mv docs/rmarkdown-cookbook.pdf _book/rmarkdown-cookbook-full.pdf &&\
+	open docs/rmarkdown-cookbook-full.pdf
 
 gitbook:
-	Rscript --quiet _render.R "bookdown::gitbook"
+	Rscript --quiet _render.R "rmdja::gitbook_ja"
 
-pdf2:
-	BOOKDOWN_FULL_PDF=false Rscript --quiet _render.R "bookdown::pdf_book"
+#pdf2:
+#	BOOKDOWN_FULL_PDF=false Rscript --quiet _render.R "bookdown::pdf_book"
--- _render.R
+++ _render.R
@@ -3,7 +3,7 @@ formats = commandArgs(TRUE)
 travis = !is.na(Sys.getenv('CI', NA))
 
 # provide default formats if necessary
-if (length(formats) == 0) formats = c('bookdown::pdf_book', 'bookdown::gitbook')
+if (length(formats) == 0) formats = c('rmdja::pdf_book_ja', 'rmdja::gitbook_ja')
 # render the book to all formats unless they are specified via command-line args
 for (fmt in formats) {
   cmd = sprintf("bookdown::render_book('index.Rmd', '%s', quiet = %s)", fmt, quiet)
@@ -13,7 +13,7 @@ for (fmt in formats) {
 unlink('rmarkdown-cookbook.log')
 
 r = '<body onload="window.location = \'https://bookdown.org/yihui\'+location.pathname">'
-if (travis) for (f in list.files('_book', '[.]html$', full.names = TRUE)) {
+if (travis) for (f in list.files('docs', '[.]html$', full.names = TRUE)) {
   x = readLines(f)
   if (length(i <- grep('^\\s*<body>\\s*$', x)) == 0) next
   # patch HTML files in gh-pages if built on Travis, to redirect to bookdown.org
@@ -21,11 +21,11 @@ if (travis) for (f in list.files('_book', '[.]html$', full.names = TRUE)) {
   writeLines(x, f)
 }
 
-redirect = function(from, to) {
+redirect = function(from, to, rootdir = "docs") {
   to = paste0('https://bookdown.org/yihui/rmarkdown-cookbook/', to)
   writeLines(sprintf(
     '<html><head><meta http-equiv="refresh" content="0; URL=\'%s\'" /></head><body><script>window.location = "%s";</script></html>', to, to
-  ), paste0('_book/', from))
+  ), file.path(rootdir, from))
 }
 
 redirect('r-markdown-components.html', 'rmarkdown-process.html')
