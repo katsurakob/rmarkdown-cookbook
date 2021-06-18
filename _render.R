fc140170684c7f029aaa7cf6b2e781c22459f7a0
Katagiri, Satoshi <katagiri.stsh@gmail.com>
2021-06-18 12:00:34 +0900

    Modify for i10n

--- _render.R
+++ _render.R
@@ -13,8 +13,8 @@ unlink('rmarkdown-cookbook.log')
 
 
 # creates redirects
-redirect = function(from, to) {
-  to = paste0('https://bookdown.org/yihui/rmarkdown-cookbook/', to)
+redirect = function(from, to, rootdir = "docs") {
+  # to = paste0('https://gedevan-aleksizde.github.io/rmarkdown-cookbook/', to)  # maybe relative path is preferable
   writeLines(sprintf(
     '<html><head><meta http-equiv="refresh" content="0; URL=\'%s\'" /></head><body><script>window.location = "%s";</script></html>', to, to
   ), file.path(rootdir, from))
@@ -23,22 +23,22 @@ redirect = function(from, to) {
 redirect('r-markdown-components.html', 'rmarkdown-process.html')
 redirect('acknowledgements.html', 'acknowledgments.html')
 
-if (length(formats) > 1) {
-  if (!is.na(Sys.getenv('CI', NA))) {
-    xfun::pkg_load2("rsconnect")
-    # On CI connect to server, using API KEY and deploy using appId
-    rsconnect::addConnectServer('https://bookdown.org', 'bookdown.org')
-    rsconnect::connectApiUser(
-      account = 'GHA', server = 'bookdown.org',
-      apiKey = Sys.getenv('CONNECT_API_KEY')
-    )
-    rsconnect::deploySite(
-      appId = Sys.getenv('CONTENT_ID'),
-      server = 'bookdown.org',
-      render = 'none', logLevel = 'verbose',
-      forceUpdate = TRUE)
-  } else if (Sys.getenv('USER') == 'yihui') {
-    # for local deployment when rsconnect/ is available
-    bookdown::publish_book('rmarkdown-cookbook', server = 'bookdown.org', render = 'none')
-  }
-}
+# if (length(formats) > 1) {
+#   if (!is.na(Sys.getenv('CI', NA))) {
+#     xfun::pkg_load2("rsconnect")
+#     # On CI connect to server, using API KEY and deploy using appId
+#     rsconnect::addConnectServer('https://bookdown.org', 'bookdown.org')
+#     rsconnect::connectApiUser(
+#       account = 'GHA', server = 'bookdown.org',
+#       apiKey = Sys.getenv('CONNECT_API_KEY')
+#     )
+#     rsconnect::deploySite(
+#       appId = Sys.getenv('CONTENT_ID'),
+#       server = 'bookdown.org',
+#       render = 'none', logLevel = 'verbose',
+#       forceUpdate = TRUE)
+#   } else if (Sys.getenv('USER') == 'yihui') {
+#     # for local deployment when rsconnect/ is available
+#     bookdown::publish_book('rmarkdown-cookbook', server = 'bookdown.org', render = 'none')
+#   }
+# }
